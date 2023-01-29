05540b786a58852bea32f7ad4729a85f24f5b7ab
minhazpro3 <minhazpro1@gmail.com>
2023-01-30 03:35:37 +0600

    project setup

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const cors = require("cors");
+const { connectToServer } = require("./utils/dbConnection");
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 5000;
+app.use(cors());
+app.use(express.json());
+
+connectToServer((err) => {
+  if (!err) {
+    app.listen(port, () => {
+      console.log("HEY connected database", port);
+    });
+  } else {
+    console.log("You have to  sure connect!!");
+  }
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.all("*", (req, res) => {
+  res.send(" Route in not found");
+});
---
+++ utils/dbConnection.js
@@ -0,0 +1,28 @@
+const { MongoClient } = require("mongodb");
+require("dotenv").config();
+const connectionString = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.z45ex.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(connectionString, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+let dbConnection;
+
+module.exports = {
+  connectToServer: function (callback) {
+    client.connect(function (err, db) {
+      if (err || !db) {
+        return callback(err);
+      }
+
+      dbConnection = db.db("power_hack-bill");
+      console.log("Successfully connected to MongoDB.");
+
+      return callback();
+    });
+  },
+
+  getDb: function () {
+    return dbConnection;
+  },
+};
